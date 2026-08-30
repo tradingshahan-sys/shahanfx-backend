da3701ee465bca549714856b9e13909116624dca
tradingshahan-sys <tradingshahan@gmail.com>
2026-08-31 02:45:05 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,24 +1,6 @@
-export default function handler(req, res) {
-if (req.method === "OPTIONS") {
-res.setHeader("Access-Control-Allow-Origin", "*");
-res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-return res.status(200).end();
-}
-
-res.setHeader("Access-Control-Allow-Origin", "*");
-res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-if (req.method === "POST") {
-return res.status(200).json({
+module.exports = (req, res) => {
+res.status(200).json({
 success: true,
 answer: "POST_TEST_OK"
 });
-}
-
-return res.status(405).json({
-success: false,
-error: "تەنها POST ڕێگەپێدراوە"
-});
-}
+};
